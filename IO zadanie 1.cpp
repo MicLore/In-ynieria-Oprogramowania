af314237809820ba4781f8758b9a627e0b45e558
MicLore <115951082+MicLore@users.noreply.github.com>
2023-10-19 14:24:01 +0200

    Zadanie 1

---
+++ IO zadanie 1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    int licz = 300;
+    while (licz < 1201) {
+        if (licz % 2 == 1) {
+            cout << licz << " ";
+        }
+        else if (licz % 400 == 0) {
+            cout << endl;
+        }
+        else if (licz % 100 == 0) {
+            cout << "*** ";
+        }
+        licz++;
+    }
+}
