77af3ff5c723e9975e7206795e18fa6a0df06612
MicLore <115951082+MicLore@users.noreply.github.com>
2023-10-26 14:44:14 +0200

    Add files via upload

---
+++ Diagram aktywności ustawień/Diagram aktywności ustawień/Diagram aktywności ustawień.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "Ustawienia.h"
+
+using namespace std;
+int main()
+{
+    Ustawienia nowe;
+    nowe.wyswietl();
+}
---
+++ Diagram aktywności ustawień/Diagram aktywności ustawień/Ustawienia.cpp
@@ -0,0 +1,2 @@
+#include "Ustawienia.h"
+using namespace std;
