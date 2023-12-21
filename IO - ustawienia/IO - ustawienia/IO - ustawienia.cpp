a9355b64b64c06b11b1e34018dc2c58917de7786
MicLore <115951082+MicLore@users.noreply.github.com>
2023-12-21 10:47:37 +0100

    Add files via upload

---
+++ IO - ustawienia/IO - ustawienia/IO - ustawienia.cpp
@@ -0,0 +1,198 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+#define ROZMIAR 8	//liczba opcji dostępnych w menu ustawień + 1
+
+class Ustawienia
+{
+	//int zestawOpcji[ROZMIAR];
+
+public:
+	int zestawOpcji[ROZMIAR];
+
+	void menu() {
+		system("cls");
+		cout << "==========================================" << endl;
+		cout << "                 ORGANIZER                " << endl;
+		cout << "==========================================" << endl;
+		cout << endl;
+		cout << "MENU: " << endl;
+		cout << "[1] Wyświetl kalendarz" << endl;
+		cout << "[2] Zapisane zdarzenia" << endl;
+		cout << "[3] Ustawienia" << endl;
+		cout << "[ESC] Wyjście" << endl;
+		char x = _getch();
+		if (x == 27) {
+			system("cls");
+			cout << "Dziękujemy za korzystanie z naszego organizera  :)" << endl;
+			Sleep(1000);
+			exit(0);
+		}
+		else if (x == '3') {
+			system("cls");
+			wczytajUstawienia();
+			wyswietl();
+		}
+		else {
+			system("cls");
+			cout << "błąd" << endl;
+			Sleep(500);
+			menu();
+		}
+	}
+
+	void wczytajUstawienia() {
+		fstream ustawienia;
+		ustawienia.open("ustawienia.txt", ios::in);
+		/*
+		if (ustawienia.good() == false)
+		{
+			menu();
+		}
+		string line;
+		int iterator = 0;
+		while (getline(ustawienia, line))
+		{
+			zestawOpcji[iterator] = stoi(line);
+			iterator++;
+		}
+		ustawienia.close();*/
+		zestawOpcji[0] = 0;
+		zestawOpcji[1] = 3;
+		zestawOpcji[2] = 3;
+		zestawOpcji[3] = 0;
+		zestawOpcji[4] = 3;
+		zestawOpcji[5] = 0;
+		zestawOpcji[6] = 3;
+		zestawOpcji[7] = 3;
+	}
+
+	void zapiszUstawienia() {
+		for (int i = 0; i < ROZMIAR; i++) {
+			if (zestawOpcji[i] == 1) {
+				zestawOpcji[i] = 0;
+			}
+			else if (zestawOpcji[i] == 2) {
+				zestawOpcji[i] = 3;
+			}
+		}
+		fstream ustawienia;
+		ustawienia.open("ustawienia.txt", ios::out);
+		for (int i = 0; i < ROZMIAR; i++) {
+			ustawienia << zestawOpcji[i];
+			ustawienia << char(10);
+		}
+		ustawienia.close();
+	}
+
+
+	void wyswietl() {
+		cout << "Ustawienia" << endl << endl;
+		for (int i = 0; i <= ROZMIAR; i++) {
+
+			switch (zestawOpcji[i])
+			{
+			case 0:
+				cout << "[ ] Opcja nr. " << i + 1 << endl;
+				break;
+			case 1:
+				cout << "[-] Opcja nr. " << i + 1 << endl;
+				break;
+			case 2:
+				cout << "[*] Opcja nr. " << i + 1 << endl;
+				break;
+			case 3:
+				cout << "[X] Opcja nr. " << i + 1 << endl;
+				break;
+			default:
+				cout << endl << "Opcje zaznaczone  [X]  są aktywne." << endl;
+				cout << "Opcje zaznaczone  [*]  są wybrane do aktywacji." << endl;
+				cout << "Opcje zaznaczone  [-]  są wybrane do dezaktywacji." << endl;
+				break;
+			}				
+		}
+		int x = _getch();
+
+		if (x == 27) {
+			system("cls");
+			cout << "Czy chcesz zapisać ustawienia?" << endl << " T / N" << endl;
+			char tru = _getch();
+			if (tru == 't') {
+				system("cls");
+				zapiszUstawienia();
+				cout << "Zapisano ustawienia" << endl;
+				Sleep(600);
+				system("cls");
+				menu();
+			}
+			else if (tru == 'n') {
+				system("cls");
+				cout << "Nie zapisano ustawień" << endl;
+				Sleep(600);
+				system("cls");
+				menu();
+			}
+			else {
+				cout << "błąd" << endl;
+				Sleep(600);
+				system("cls");
+				wyswietl();
+			}
+		}
+		else if ((x - 49) == 7) {
+			system("cls");
+			cout << "Czy posiadasz uprawnienia administratora?" << endl << " T / N" << endl;
+			char tru = _getch();
+			if (tru == 't') {
+				system("cls");
+				zmienUstawienie(x - 49);
+				cout << "Zmieniono opcję" << endl;
+				Sleep(600);
+				system("cls");
+				wyswietl();
+			}
+			else if (tru == 'n') {
+				system("cls");
+				cout << "Nie masz wystarczających uprawnień" << endl;
+				Sleep(600);
+				system("cls");
+				wyswietl();
+			}
+		}
+		else {
+			zmienUstawienie(x-49);
+		}
+	}
+	void zmienUstawienie(int numer) {
+
+		switch (zestawOpcji[numer])
+		{
+		case 0:
+			zestawOpcji[numer] = 2;
+			break;
+		case 1:
+			zestawOpcji[numer] = 3;
+			break;
+		case 2:
+			zestawOpcji[numer] = 0;
+			break;
+		case 3:
+			zestawOpcji[numer] = 1;
+			break;
+		}
+		system("cls");
+		//wyswietl();
+	}
+
+};
+
+int main()
+{
+	Ustawienia ust;
+	ust.menu();
+}
---
+++ IO - ustawienia/draże/draże.cpp
@@ -0,0 +1,109 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\IO - ustawienia\IO - ustawienia.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace draże
+{
+	TEST_CLASS(draże)
+	{
+	public:
+		
+        TEST_METHOD(UstawieniaObjectCreation)
+        {
+            // Arrange
+            Ustawienia ust;
+
+            // Act - No specific action needed, as the instance is already created
+
+            // Assert
+            Assert::IsNotNull(&ust); // Check if the instance of Ustawienia is not null
+        }
+        TEST_METHOD(ReadSettingsFromFile)
+        {
+            // Arrange
+            Ustawienia ust;
+            std::string testFilename = "test_ustawienia.txt"; // Test file name
+
+
+            // Act
+            ust.wczytajUstawienia(); // Read settings from the test file
+
+            // Assert
+            // Verify if the values read by the function match the expected values
+            Assert::AreEqual(0, ust.zestawOpcji[0]);
+            Assert::AreEqual(3, ust.zestawOpcji[1]);
+            Assert::AreEqual(3, ust.zestawOpcji[2]);
+            Assert::AreEqual(0, ust.zestawOpcji[3]);
+            Assert::AreEqual(3, ust.zestawOpcji[4]);
+            Assert::AreEqual(0, ust.zestawOpcji[5]);
+            Assert::AreEqual(3, ust.zestawOpcji[6]);
+            Assert::AreEqual(3, ust.zestawOpcji[7]);
+
+            // Optional: Clean up by removing the test file
+            //std::remove(testFilename.c_str());
+        }
+        TEST_METHOD(SaveSettingsToFile)
+        {
+            // Arrange
+            Ustawienia ust;
+            std::string testFilename = "test_ustawienia_save.txt"; // Test file name
+
+            // Set some initial values in zestawOpcji
+            ust.zestawOpcji[0] = 0;
+            ust.zestawOpcji[1] = 3;
+            ust.zestawOpcji[2] = 3;
+            ust.zestawOpcji[3] = 0;
+            ust.zestawOpcji[4] = 3;
+            ust.zestawOpcji[5] = 0;
+            ust.zestawOpcji[6] = 3;
+            ust.zestawOpcji[7] = 3;
+
+            // Act
+            ust.zapiszUstawienia(); // Save settings to the test file
+
+            // Assert
+            // Verify if the values were saved correctly to the file
+            std::ifstream testFile(testFilename);
+            std::stringstream buffer;
+            buffer << testFile.rdbuf(); // Read the file content into a stringstream
+            testFile.close();
+
+            std::string fileContent = buffer.str();
+
+            // Check if each value is correctly written to the file
+            Assert::AreEqual("0\n3\n3\n0\n3\n0\n3\n3\n", fileContent.c_str());
+
+            // Optional: Clean up by removing the test file
+            std::remove(testFilename.c_str());
+        }
+        TEST_METHOD(ChangeSettingValue)
+        {
+            // Arrange
+            Ustawienia ust;
+
+            // Set initial values for testing
+            ust.zestawOpcji[0] = 0; // Initial value: 0
+
+            // Act
+            ust.zmienUstawienie(0); // Change the setting value
+
+            // Assert
+            Assert::AreEqual(2, ust.zestawOpcji[0]); // Check if the value is changed correctly
+
+            // Additional test cases for other initial values and input parameters
+            ust.zestawOpcji[1] = 1; // Initial value: 1
+            ust.zmienUstawienie(1); // Change the setting value
+            Assert::AreEqual(3, ust.zestawOpcji[1]); // Check if the value is changed correctly
+
+            ust.zestawOpcji[2] = 2; // Initial value: 2
+            ust.zmienUstawienie(2); // Change the setting value
+            Assert::AreEqual(0, ust.zestawOpcji[2]); // Check if the value is changed correctly
+
+            ust.zestawOpcji[3] = 3; // Initial value: 3
+            ust.zmienUstawienie(3); // Change the setting value
+            Assert::AreEqual(1, ust.zestawOpcji[3]); // Check if the value is changed correctly
+        }
+	};
+}
